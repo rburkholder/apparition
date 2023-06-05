628b1ff94d912d9ecf01f736903e89219d3ef487
Raymond Burkholder <raymond@burkholder.net>
2023-06-05 00:03:25 -0600

    src/AppApparition - skeleton of apparition

---
+++ src/AppApparition.cpp
@@ -0,0 +1,26 @@
+/************************************************************************
+ * Copyright(c) 2022, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    AppApparition.cpp
+ * Author:  raymond@burkholder.net
+ * Project: Apparition
+ * Created: 2023/06/04 23:52:32
+ */
+
+#include "AppApparition.hpp"
+
+int main( int argc, char* argv[] ) {
+
+}
---
+++ src/AppApparition.hpp
@@ -0,0 +1,21 @@
+/************************************************************************
+ * Copyright(c) 2022, One Unified. All rights reserved.                 *
+ * email: info@oneunified.net                                           *
+ *                                                                      *
+ * This file is provided as is WITHOUT ANY WARRANTY                     *
+ *  without even the implied warranty of                                *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                *
+ *                                                                      *
+ * This software may not be used nor distributed without proper license *
+ * agreement.                                                           *
+ *                                                                      *
+ * See the file LICENSE.txt for redistribution information.             *
+ ************************************************************************/
+
+/*
+ * File:    AppApparition.hpp
+ * Author:  raymond@burkholder.net
+ * Project: Apparition
+ * Created: 2023/06/04 23:52:32
+ */
+
