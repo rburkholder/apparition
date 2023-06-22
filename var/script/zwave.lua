995eb1c290ff35380eca0761292ecf6aa2e72247
Raymond Burkholder <raymond@burkholder.net>
2023-06-22 11:49:46 -0600

    var/script/zwave.lua * proof of concept:     change philips hue on/off from zooz scene controller     migrate to control via state change event

--- var/script/zwave.lua
+++ var/script/zwave.lua
@@ -1,16 +1,16 @@
--- file:   zwave.lua
--- author: raymond@burkholder.net
+-- file:    zwave.lua
+-- author:  raymond@burkholder.net
 -- creawted 2023/06/14 16:24:04
 
 -- local m = require("strict")
 
-description = 'watches zwave value to domoticz/in'
+description = 'watches zwave in topic domoticz/in '
 
 local topic = 'domoticz/in/#'
 local object_ptr = 0
 
 package.path='' -- can not have ?.so in script path
-package.cpath='lib/lua/?.so' -- dedicate to custom direcotry for now
+package.cpath='lib/lua/?.so' -- dedicate to custom directory for now
 local cjson = require( 'cjson' )
 local json = cjson.new()
 
@@ -59,12 +59,99 @@ zwave_value = function( json_, zwave_ix_dev_, zwave_ix_var_, sensor_ )
       value = false -- test if this can be 'visited' easier
     end
 
-    if ( '37' == zwave_ix_var_ ) and ( 'duration' == sensor_ ) then
+    if 'table' ~= type(value) then
+      record = {
+        sensor_, value, units -- empty units for now
+      }
+      data[ #data + 1 ] = record
+
+      mqtt_device_data( object_ptr, location, device, #data, data );
+    end
+  end
+
+end
+
+-- zooz 5 button scene controller - binary button
+zwave_37 = function( json_, zwave_ix_dev_, zwave_ix_var_, sensor_ )
+  local data = {}
+
+  local device = json_[ 'nodeName' ]
+  local location = json_[ 'nodeLocation' ]
+
+  if ( nil == device ) or ( nil == location ) then
+    io.write( '*** no device or location: ' .. sensor_ .. ', ' .. zwave_ix_dev_ .. ', ' .. zwave_ix_var_ .. '\n' )
+  else
+    local units = ''
+    local value = json_[ 'value' ]
+    if nil == value then
+      -- value = 'n/a'
+      value = false -- test if this can be 'visited' easier
+    end
+
+    if 'duration' == sensor_ then
       -- might be able to generalize this to 37 for the variable type across devices
         -- will need to listen to discovery & confirm
       value = value[ 'value' ]
       units = value[ 'unit' ]
+    elseif 'targetValue' == sensor_ then
+      -- TODO: use event registration instead
+      if 'boolean' == type(value) then
+        if value then
+          io.write( 'turn on light\n' )
+          local topic1 = 'zigbee/1/den_right/set'
+          local topic2 = 'zigbee/1/den_left/set'
+          local message = '{"state":"ON"}'
+          mqtt_publish( object_ptr, topic1, message )
+          mqtt_publish( object_ptr, topic2, message )
+        else
+          io.write( 'turn off light\n')
+          local topic1 = 'zigbee/1/den_right/set'
+          local topic2 = 'zigbee/1/den_left/set'
+          local message = '{"state":"OFF"}'
+          mqtt_publish( object_ptr, topic1, message )
+          mqtt_publish( object_ptr, topic2, message )
+        end
+      else
+        io.write( 'targetValue is not boolean: ' .. value .. '\n' )
+      end
     end
+
+    if 'table' ~= type(value) then
+      record = {
+        sensor_, value, units -- empty units for now
+      }
+      data[ #data + 1 ] = record
+
+      mqtt_device_data( object_ptr, location, device, #data, data );
+    end
+  end
+
+end
+
+-- zooz 5 button scene controller - scene control
+zwave_91 = function( json_, zwave_ix_dev_, zwave_ix_var_, sensor_ )
+  io.write( '*** to be processed: scene controller dev ' .. zwave_ix_dev_ .. '\n' )
+  -- scene00x:
+  -- 0:     key pressed
+  -- 1:     key released
+  -- 2:     key held down
+  -- false: end of state change (undefined)
+  local data = {}
+
+  local device = json_[ 'nodeName' ]
+  local location = json_[ 'nodeLocation' ]
+
+  if ( nil == device ) or ( nil == location ) then
+    io.write( '*** zwave 91 - no device or location: ' .. sensor_ .. ', ' .. zwave_ix_dev_ .. ', ' .. zwave_ix_var_ .. '\n' )
+  else
+    local units = ''
+    local value = json_[ 'value' ]
+    if nil == value then
+      -- value = 'n/a'
+      value = false -- ie, no activity at controller
+    end
+
+    -- TODO: send command to zigbee
 
     record = {
       sensor_, value, units -- empty units for now
@@ -74,16 +161,6 @@ zwave_value = function( json_, zwave_ix_dev_, zwave_ix_var_, sensor_ )
     mqtt_device_data( object_ptr, location, device, #data, data );
   end
 
-end
-
--- zooz 5 button scene controller
-zwave_91 = function( json_, zwave_ix_dev, sensor )
-  io.write( '*** to be processed: scene controller dev ' .. zwave_ix_dev .. '\n' )
-  -- scene00x:
-  -- 0:     key pressed
-  -- 1:     key released
-  -- 2:     key held down
-  -- false: end of state change (undefined)
 end
 
 zwave_last_active = function( json_, zwave_ix_dev )
@@ -173,10 +250,12 @@ mqtt_in = function( topic_, message_ )
 
   -- will ultimately require the discovery information to decode & describe the stream
 
-  if '91' == zwave_ix_var then
+  if '91' == zwave_ix_var then -- zooz scenes
     jvalues = json.decode( message_ )
-    --zwave_91( jvalues, zwave_ix_var, sensor )
-    zwave_value( jvalues, zwave_ix_dev, zwave_ix_var, sensor )
+    zwave_91( jvalues, zwave_ix_dev, zwave_ix_var, sensor )
+  elseif '37' == zwave_ix_var then
+    jvalues = json.decode( message_ )
+    zwave_37( jvalues, zwave_ix_dev, zwave_ix_var, sensor )
   else
     if 7 == ix then
       -- process words
