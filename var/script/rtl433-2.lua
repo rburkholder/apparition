1bbf7502f29cb4e05e789e43a20bf1a0b2604374
Raymond Burkholder <raymond@burkholder.net>
2023-06-24 13:55:06 -0600

    var/script/rtl433-2.lua - disable diagnostic output

--- var/script/rtl433-2.lua
+++ var/script/rtl433-2.lua
@@ -57,28 +57,28 @@ dsc = function( json_ )
   local info = dsc_info[ id ]
   if nil ~= info then
     local data = {}
-    extract2( json_, data, "closed",      "")
-    extract2( json_, data, "tamper",      "")
-    extract2( json_, data, "status",      "")
-    extract3( json_, data, "battery_ok",    "",     "battery_state" )
-    extract2( json_, data, "rssi",          "dBm")
-    extract2( json_, data, "snr",           "")
-    extract2( json_, data, "noise",         "dBm")
+    extract2( json_, data, "closed",     "")
+    extract2( json_, data, "tamper",     "")
+    extract2( json_, data, "status",     "")
+    extract3( json_, data, "battery_ok", "", "battery_state" )
+    extract2( json_, data, "rssi",       "dBm")
+    extract2( json_, data, "snr",        "")
+    extract2( json_, data, "noise",      "dBm")
     mqtt_device_data( object_ptr, info[ 1 ] , info[ 2 ], #data, data );
   else
     local data = {}
-    extract3( json_, data, "battery_ok",    "",     "battery_state" )
-    extract2( json_, data, "rssi",          "dBm")
-    extract2( json_, data, "snr",           "")
-    extract2( json_, data, "noise",         "dBm")
+    extract3( json_, data, "battery_ok", "", "battery_state" )
+    extract2( json_, data, "rssi",       "dBm")
+    extract2( json_, data, "snr",        "")
+    extract2( json_, data, "noise",      "dBm")
     mqtt_device_data( object_ptr, "top floor?", "dsc", #data, data );
   end
 end
 
 local thermapro_location = {}
-thermapro_location[ 128 ] = 'walkway' -- channel 3
-thermapro_location[ 163 ] = 'garage' -- channel 1
-thermapro_location[ 100 ] = 'basement'  -- channel 2
+thermapro_location[ 163 ] = 'garage'   -- channel 1
+thermapro_location[ 100 ] = 'basement' -- channel 2
+thermapro_location[ 128 ] = 'walkway'  -- channel 3
 
 -- rtl433/2 {"time":"2023-06-22 20:17:22","model":"Prologue-TH","subtype":9,"id":128,"channel":3,"battery_ok":1,"temperature_C":23.5,"humidity":33,"button":0,"mod":"ASK","freq":433.93552,"rssi":-2.04417,"snr":20.27732,"noise":-22.3215}
 thermapro = function( json_ )
@@ -106,10 +106,10 @@ mqtt_in = function( topic_, message_ )
   local model = jvalues[ 'model' ]
   if model then
     if 'DSC-Security' == model then
-      io.write( "mqtt_in ".. topic_ .. ": ".. message_.. '\n' )
+      -- io.write( "mqtt_in ".. topic_ .. ": ".. message_.. '\n' )
       dsc( jvalues )
     elseif 'Prologue-TH' == model then
-      io.write( "mqtt_in ".. topic_ .. ": ".. message_.. '\n' )
+      -- io.write( "mqtt_in ".. topic_ .. ": ".. message_.. '\n' )
       thermapro( jvalues )
     end
   end
