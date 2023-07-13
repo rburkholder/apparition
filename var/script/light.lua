7f5f8c946a60b4993a93c58dcb1fcb66284b8d5a
Raymond Burkholder <raymond@burkholder.net>
2023-07-13 10:58:51 -0600

    var/script/light.lua - add side entry scene controller

--- var/script/light.lua
+++ var/script/light.lua
@@ -14,6 +14,9 @@ package.cpath='lib/lua/?.so' -- dedicate to custom directory for now
 local cjson = require( 'cjson' )
 local json = cjson.new()
 
+local name_laundry = 'side entry'
+local name_den = 'den'
+
 attach = function ( object_ptr_ )
   -- use os.getenv for username, password info
   object_ptr = object_ptr_
@@ -23,6 +26,12 @@ attach = function ( object_ptr_ )
   event_register_add( object_ptr, "den", "scene01", "scene002" )
   event_register_add( object_ptr, "den", "scene01", "scene003" )
   event_register_add( object_ptr, "den", "scene01", "scene004" )
+
+  event_register_add( object_ptr, name_laundry, "scene02", "targetValue" )
+  event_register_add( object_ptr, name_laundry, "scene02", "scene001" )
+  event_register_add( object_ptr, name_laundry, "scene02", "scene002" )
+  event_register_add( object_ptr, name_laundry, "scene02", "scene003" )
+  event_register_add( object_ptr, name_laundry, "scene02", "scene004" )
   mqtt_start_topic( object_ptr, topic ); -- needed?
 
 end
@@ -34,18 +43,23 @@ detach = function ( object_ptr_ )
   event_register_del( object_ptr, "den", "scene01", "scene002" )
   event_register_del( object_ptr, "den", "scene01", "scene003" )
   event_register_del( object_ptr, "den", "scene01", "scene004" )
+
+  event_register_del( object_ptr, name_laundry, "scene02", "targetValue" )
+  event_register_del( object_ptr, name_laundry, "scene02", "scene001" )
+  event_register_del( object_ptr, name_laundry, "scene02", "scene002" )
+  event_register_del( object_ptr, name_laundry, "scene02", "scene003" )
+  event_register_del( object_ptr, name_laundry, "scene02", "scene004" )
   mqtt_disconnect( object_ptr )
   object_ptr = nil
 end
 
 event_sensor_changed = function( location_, device_, sensor_, value_ )
-  if false then
+  if true then
     io.write(
       "light.lua event_sensor_changed: "
       .. location_ .. ','
       .. device_ .. ','
       .. sensor_ .. ','
-      .. value_ .. ','
       .. type(value_) ..
       '\n'
       )
@@ -59,21 +73,37 @@ event_sensor_changed = function( location_, device_, sensor_, value_ )
 
   -- will need to auto-compose strings from event/sensor dictionary?
   -- perform a lookup on a table to determine action to take?
+
+
   if 'targetValue' == sensor_ then
-    if false == value_ then
-      -- io.write( 'turn off den light\n')
-      local topic1 = 'zigbee/1/light01/den/set'
-      local topic2 = 'zigbee/1/light02/den/set'
-      local message = '{"state":"OFF"}'
-      mqtt_publish( object_ptr, topic1, message )
-      mqtt_publish( object_ptr, topic2, message )
-    elseif true == value_ then
-      -- io.write( 'turn on den light\n' )
-      local topic1 = 'zigbee/1/light01/den/set'
-      local topic2 = 'zigbee/1/light02/den/set'
-      local message = '{"state":"ON"}'
-      mqtt_publish( object_ptr, topic1, message )
-      mqtt_publish( object_ptr, topic2, message )
+    if 'scene01' == device_ then
+      if false == value_ then
+        -- io.write( 'turn off den light\n')
+        local topic1 = 'zigbee/1/light01/den/set'
+        local topic2 = 'zigbee/1/light02/den/set'
+        local message = '{"state":"OFF"}'
+        mqtt_publish( object_ptr, topic1, message )
+        mqtt_publish( object_ptr, topic2, message )
+      elseif true == value_ then
+        -- io.write( 'turn on den light\n' )
+        local topic1 = 'zigbee/1/light01/den/set'
+        local topic2 = 'zigbee/1/light02/den/set'
+        local message = '{"state":"ON"}'
+        mqtt_publish( object_ptr, topic1, message )
+        mqtt_publish( object_ptr, topic2, message )
+      end
+    elseif 'scene02' == device_ then
+      if false == value_ then
+        -- io.write( 'turn off laundry light\n')
+        local topic1 = 'zigbee/1/light03/laundry/set'
+        local message = '{"state":"OFF"}'
+        mqtt_publish( object_ptr, topic1, message )
+      elseif true == value_ then
+        -- io.write( 'turn on laundry light\n' )
+        local topic1 = 'zigbee/1/light03/laundry/set'
+        local message = '{"state":"ON"}'
+        mqtt_publish( object_ptr, topic1, message )
+      end
     end
   elseif 'scene001' == sensor_ then -- eating area light
     if 0 == value_ then
@@ -90,12 +120,12 @@ event_sensor_changed = function( location_, device_, sensor_, value_ )
   elseif 'scene002' == sensor_ then -- back hallway light
     if 0 == value_ then
       -- io.write( 'turn on back entry light\n')
-      local topic1 = 'zigbee/1/light03/back_entry/set'
+      local topic1 = 'zigbee/1/light03/laundry/set'
       local message = '{"state":"ON"}'
       mqtt_publish( object_ptr, topic1, message )
     elseif 2 == value_ then
       -- io.write( 'turn off back entry light\n')
-      local topic1 = 'zigbee/1/light03/back_entry/set'
+      local topic1 = 'zigbee/1/light03/laundry/set'
       local message = '{"state":"OFF"}'
       mqtt_publish( object_ptr, topic1, message )
     end
@@ -103,7 +133,7 @@ event_sensor_changed = function( location_, device_, sensor_, value_ )
     -- io.write( 'turn on all lights\n' )
     local topic1 = 'zigbee/1/light01/den/set'
     local topic2 = 'zigbee/1/light02/den/set'
-    local topic3 = 'zigbee/1/light03/back_entry/set'
+    local topic3 = 'zigbee/1/light03/laundry/set'
     local topic4 = 'zigbee/1/light04/eating_area/set'
     local message = '{"state":"ON"}'
     -- TODO: update targetvalue on scene01 to maintain consistency
@@ -115,7 +145,7 @@ event_sensor_changed = function( location_, device_, sensor_, value_ )
     -- io.write( 'turn off all lights\n' )
     local topic1 = 'zigbee/1/light01/den/set'
     local topic2 = 'zigbee/1/light02/den/set'
-    local topic3 = 'zigbee/1/light03/back_entry/set'
+    local topic3 = 'zigbee/1/light03/laundry/set'
     local topic4 = 'zigbee/1/light04/eating_area/set'
     local message = '{"state":"OFF"}'
     -- TODO: update targetvalue on scene01 to maintain consistency
