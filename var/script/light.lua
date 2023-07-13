39ff5f762e6c418a186a43f2b10b26cc7528fa21
Raymond Burkholder <raymond@burkholder.net>
2023-07-13 13:44:39 -0600

    var/script/light.lua - add third zooz scene controller

--- var/script/light.lua
+++ var/script/light.lua
@@ -14,44 +14,234 @@ package.cpath='lib/lua/?.so' -- dedicate to custom directory for now
 local cjson = require( 'cjson' )
 local json = cjson.new()
 
-local name_laundry = 'side entry'
-local name_den = 'den'
+local name_den = 'den'  -- scene01
+local name_laundry = 'side entry' -- scene02
+local name_eating_area = 'eating area' -- scene 03
 
 attach = function ( object_ptr_ )
   -- use os.getenv for username, password info
   object_ptr = object_ptr_
   mqtt_connect( object_ptr )
-  event_register_add( object_ptr, "den", "scene01", "targetValue" )
-  event_register_add( object_ptr, "den", "scene01", "scene001" )
-  event_register_add( object_ptr, "den", "scene01", "scene002" )
-  event_register_add( object_ptr, "den", "scene01", "scene003" )
-  event_register_add( object_ptr, "den", "scene01", "scene004" )
+
+  event_register_add( object_ptr, name_den, "scene01", "targetValue" )
+  event_register_add( object_ptr, name_den, "scene01", "scene001" )
+  event_register_add( object_ptr, name_den, "scene01", "scene002" )
+  event_register_add( object_ptr, name_den, "scene01", "scene003" )
+  event_register_add( object_ptr, name_den, "scene01", "scene004" )
 
   event_register_add( object_ptr, name_laundry, "scene02", "targetValue" )
   event_register_add( object_ptr, name_laundry, "scene02", "scene001" )
   event_register_add( object_ptr, name_laundry, "scene02", "scene002" )
   event_register_add( object_ptr, name_laundry, "scene02", "scene003" )
   event_register_add( object_ptr, name_laundry, "scene02", "scene004" )
+
+  event_register_add( object_ptr, name_eating_area, "scene03", "targetValue" )
+  event_register_add( object_ptr, name_eating_area, "scene03", "scene001" )
+  event_register_add( object_ptr, name_eating_area, "scene03", "scene002" )
+  event_register_add( object_ptr, name_eating_area, "scene03", "scene003" )
+  event_register_add( object_ptr, name_eating_area, "scene03", "scene004" )
+
   mqtt_start_topic( object_ptr, topic ); -- needed?
 
 end
 
 detach = function ( object_ptr_ )
   mqtt_stop_topic( object_ptr, topic ) -- needed?
-  event_register_del( object_ptr, "den", "scene01", "targetValue" )
-  event_register_del( object_ptr, "den", "scene01", "scene001" )
-  event_register_del( object_ptr, "den", "scene01", "scene002" )
-  event_register_del( object_ptr, "den", "scene01", "scene003" )
-  event_register_del( object_ptr, "den", "scene01", "scene004" )
+
+  event_register_del( object_ptr, name_den, "scene01", "targetValue" )
+  event_register_del( object_ptr, name_den, "scene01", "scene001" )
+  event_register_del( object_ptr, name_den, "scene01", "scene002" )
+  event_register_del( object_ptr, name_den, "scene01", "scene003" )
+  event_register_del( object_ptr, name_den, "scene01", "scene004" )
 
   event_register_del( object_ptr, name_laundry, "scene02", "targetValue" )
   event_register_del( object_ptr, name_laundry, "scene02", "scene001" )
   event_register_del( object_ptr, name_laundry, "scene02", "scene002" )
   event_register_del( object_ptr, name_laundry, "scene02", "scene003" )
   event_register_del( object_ptr, name_laundry, "scene02", "scene004" )
+
+  event_register_del( object_ptr, name_eating_area, "scene03", "targetValue" )
+  event_register_del( object_ptr, name_eating_area, "scene03", "scene001" )
+  event_register_del( object_ptr, name_eating_area, "scene03", "scene002" )
+  event_register_del( object_ptr, name_eating_area, "scene03", "scene003" )
+  event_register_del( object_ptr, name_eating_area, "scene03", "scene004" )
+
   mqtt_disconnect( object_ptr )
   object_ptr = nil
 end
+
+local light_den = function( state_ )
+  local topic1 = 'zigbee/1/light01/den/set'
+  local topic2 = 'zigbee/1/light02/den/set'
+  local message = '{"state":"' .. state_ .. '"}'
+  -- TODO: update targetvalue on scene01 to maintain consistency
+  mqtt_publish( object_ptr, topic1, message )
+  mqtt_publish( object_ptr, topic2, message )
+end
+
+local light_laundry = function( state_ )
+  local topic1 = 'zigbee/1/light03/laundry/set'
+  local message = '{"state":"' .. state_ .. '"}'
+  -- TODO: update targetvalue on scene01 to maintain consistency
+  mqtt_publish( object_ptr, topic1, message )
+end
+
+local light_eating_area = function( state_ )
+  local topic1 = 'zigbee/1/light04/eating_area/set'
+  local message = '{"state":"' .. state_ .. '"}'
+  -- TODO: update targetvalue on scene01 to maintain consistency
+  mqtt_publish( object_ptr, topic1, message )
+end
+
+-- all on
+local sensor_scene003 = function( value_ )
+  -- io.write( 'turn on all lights\n' )
+  light_den( 'ON' )
+  light_laundry( 'ON' )
+  light_eating_area( 'ON' )
+end
+
+-- all off
+local sensor_scene004 = function( value_ )
+    -- io.write( 'turn off all lights\n' )
+    light_den( 'OFF' )
+    light_laundry( 'OFF' )
+    light_eating_area( 'OFF' )
+  end
+
+-- den
+local controllor_scene01 = function( sensor_, value_ )
+
+  local scene001 = function( value_ )
+    if 0 == value_ then
+      -- io.write( 'turn on eating area light\n')
+      light_eating_area( 'ON' )
+    elseif 2 == value_ then
+      -- io.write( 'turn off eating area light\n')
+      light_eating_area( 'OFF' )
+    end
+  end
+
+  local scene002 = function( value_ )
+    if 0 == value_ then
+      -- io.write( 'turn on back entry light\n')
+      light_laundry( 'ON' )
+    elseif 2 == value_ then
+      -- io.write( 'turn off back entry light\n')
+      light_laundry( 'OFF' )
+    end
+  end
+
+  local scenes = {}
+  scenes[ 'scene001'] = scene001
+  scenes[ 'scene002'] = scene002
+  scenes[ 'scene003'] = sensor_scene003
+  scenes[ 'scene004'] = sensor_scene004
+
+  if 'targetValue' == sensor_ then
+    if false == value_ then
+      -- io.write( 'turn off den light\n')
+      light_den( 'OFF' )
+    elseif true == value_ then
+      -- io.write( 'turn on den light\n' )
+      light_den( 'ON' )
+    end
+  else
+    scene = scenes[ sensor_ ]
+    scene( value_ )
+  end
+end
+
+-- laundry
+local controllor_scene02 = function( sensor_, value_ )
+
+  local scene001 = function( value_ )
+    if 0 == value_ then
+      -- io.write( 'turn on eating area light\n')
+      light_eating_area( 'ON' )
+    elseif 2 == value_ then
+      -- io.write( 'turn off eating area light\n')
+      light_eating_area( 'OFF' )
+    end
+  end
+
+  local scene002 = function( value_ )
+    if 0 == value_ then
+      -- io.write( 'turn on den light\n')
+      light_den( 'ON' )
+    elseif 2 == value_ then
+      -- io.write( 'turn off den light\n')
+      light_den( 'OFF' )
+    end
+  end
+
+  local scenes = {}
+  scenes[ 'scene001'] = scene001
+  scenes[ 'scene002'] = scene002
+  scenes[ 'scene003'] = sensor_scene003
+  scenes[ 'scene004'] = sensor_scene004
+
+  if 'targetValue' == sensor_ then
+    if false == value_ then
+      -- io.write( 'turn off laundry light\n')
+      light_laundry( 'OFF' )
+    elseif true == value_ then
+      -- io.write( 'turn on laundry light\n' )
+      light_laundry( 'ON' )
+    end
+  else
+    scene = scenes[ sensor_ ]
+    scene( value_ )
+  end
+end
+
+-- eating area
+local controllor_scene03 = function( sensor_, value_ )
+
+  local scene001 = function( value_ )
+    if 0 == value_ then
+      -- io.write( 'turn on laundry light\n')
+      light_laundry( 'ON' )
+    elseif 2 == value_ then
+      -- io.write( 'turn off laundry light\n')
+      light_laundry( 'OFF' )
+    end
+  end
+
+  local scene002 = function( value_ )
+    if 0 == value_ then
+      -- io.write( 'turn on den light\n')
+      light_den( 'ON' )
+    elseif 2 == value_ then
+      -- io.write( 'turn off den light\n')
+      light_den( 'OFF' )
+    end
+  end
+
+  local scenes = {}
+  scenes[ 'scene001'] = scene001
+  scenes[ 'scene002'] = scene002
+  scenes[ 'scene003'] = sensor_scene003
+  scenes[ 'scene004'] = sensor_scene004
+
+  if 'targetValue' == sensor_ then
+    if false == value_ then
+      -- io.write( 'turn off eating area light\n')
+      light_eating_area( 'OFF' )
+    elseif true == value_ then
+      -- io.write( 'turn on eating area light\n' )
+      light_eating_area( 'ON' )
+    end
+  else
+    scene = scenes[ sensor_ ]
+    scene( value_ )
+  end
+end
+
+local device = {}
+device[ 'scene01' ] = controllor_scene01
+device[ 'scene02' ] = controllor_scene02
+device[ 'scene03' ] = controllor_scene03
 
 event_sensor_changed = function( location_, device_, sensor_, value_ )
   if true then
@@ -74,86 +264,8 @@ event_sensor_changed = function( location_, device_, sensor_, value_ )
   -- will need to auto-compose strings from event/sensor dictionary?
   -- perform a lookup on a table to determine action to take?
 
-
-  if 'targetValue' == sensor_ then
-    if 'scene01' == device_ then
-      if false == value_ then
-        -- io.write( 'turn off den light\n')
-        local topic1 = 'zigbee/1/light01/den/set'
-        local topic2 = 'zigbee/1/light02/den/set'
-        local message = '{"state":"OFF"}'
-        mqtt_publish( object_ptr, topic1, message )
-        mqtt_publish( object_ptr, topic2, message )
-      elseif true == value_ then
-        -- io.write( 'turn on den light\n' )
-        local topic1 = 'zigbee/1/light01/den/set'
-        local topic2 = 'zigbee/1/light02/den/set'
-        local message = '{"state":"ON"}'
-        mqtt_publish( object_ptr, topic1, message )
-        mqtt_publish( object_ptr, topic2, message )
-      end
-    elseif 'scene02' == device_ then
-      if false == value_ then
-        -- io.write( 'turn off laundry light\n')
-        local topic1 = 'zigbee/1/light03/laundry/set'
-        local message = '{"state":"OFF"}'
-        mqtt_publish( object_ptr, topic1, message )
-      elseif true == value_ then
-        -- io.write( 'turn on laundry light\n' )
-        local topic1 = 'zigbee/1/light03/laundry/set'
-        local message = '{"state":"ON"}'
-        mqtt_publish( object_ptr, topic1, message )
-      end
-    end
-  elseif 'scene001' == sensor_ then -- eating area light
-    if 0 == value_ then
-      -- io.write( 'turn on eating area light\n')
-      local topic1 = 'zigbee/1/light04/eating_area/set'
-      local message = '{"state":"ON"}'
-      mqtt_publish( object_ptr, topic1, message )
-    elseif 2 == value_ then
-      -- io.write( 'turn off eating area light\n')
-      local topic1 = 'zigbee/1/light04/eating_area/set'
-      local message = '{"state":"OFF"}'
-      mqtt_publish( object_ptr, topic1, message )
-    end
-  elseif 'scene002' == sensor_ then -- back hallway light
-    if 0 == value_ then
-      -- io.write( 'turn on back entry light\n')
-      local topic1 = 'zigbee/1/light03/laundry/set'
-      local message = '{"state":"ON"}'
-      mqtt_publish( object_ptr, topic1, message )
-    elseif 2 == value_ then
-      -- io.write( 'turn off back entry light\n')
-      local topic1 = 'zigbee/1/light03/laundry/set'
-      local message = '{"state":"OFF"}'
-      mqtt_publish( object_ptr, topic1, message )
-    end
-  elseif 'scene003' == sensor_ then -- all lights on
-    -- io.write( 'turn on all lights\n' )
-    local topic1 = 'zigbee/1/light01/den/set'
-    local topic2 = 'zigbee/1/light02/den/set'
-    local topic3 = 'zigbee/1/light03/laundry/set'
-    local topic4 = 'zigbee/1/light04/eating_area/set'
-    local message = '{"state":"ON"}'
-    -- TODO: update targetvalue on scene01 to maintain consistency
-    mqtt_publish( object_ptr, topic1, message )
-    mqtt_publish( object_ptr, topic2, message )
-    mqtt_publish( object_ptr, topic3, message )
-    mqtt_publish( object_ptr, topic4, message )
-  elseif 'scene004' == sensor_ then -- all lights off
-    -- io.write( 'turn off all lights\n' )
-    local topic1 = 'zigbee/1/light01/den/set'
-    local topic2 = 'zigbee/1/light02/den/set'
-    local topic3 = 'zigbee/1/light03/laundry/set'
-    local topic4 = 'zigbee/1/light04/eating_area/set'
-    local message = '{"state":"OFF"}'
-    -- TODO: update targetvalue on scene01 to maintain consistency
-    mqtt_publish( object_ptr, topic1, message )
-    mqtt_publish( object_ptr, topic2, message )
-    mqtt_publish( object_ptr, topic3, message )
-    mqtt_publish( object_ptr, topic4, message )
-  end
+  local controller = device[ device_ ]
+  controller( sensor_, value_ )
 
 end
 
