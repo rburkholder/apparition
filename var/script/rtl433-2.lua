a575c758c41278b84dd188d599825253ee00e99a
Raymond Burkholder <raymond@burkholder.net>
2023-07-14 21:13:27 -0600

    var/script/rtl433-2.lua - consistent parameter value

--- var/script/rtl433-2.lua
+++ var/script/rtl433-2.lua
@@ -27,7 +27,7 @@ end
 detach = function ( object_ptr_ )
   mqtt_stop_topic( object_ptr, topic )
   mqtt_disconnect( object_ptr )
-  object_ptr = 0
+  object_ptr = nil
 end
 
 local dsc_info = {}
@@ -39,25 +39,25 @@ dsc_info[ 3860837 ] = { 'family room', 'pir' } -- seems to only trigger, not res
 
 -- rtl433/2 {"time":"2023-06-22 19:37:10","model":"DSC-Security","id":3860837,"closed":0,"event":1,"tamper":0,"battery_ok":1,"xactivity":1,"xtamper1":0,"xtamper2":0,"exception":0,"esn":"3ae965","status":161,"status_hex":"a1","mic":"CRC","mod":"ASK","freq":433.93008,"rssi":-0.13023,"snr":22.19126,"noise":-22.3215}
 -- rtl433/2 {"time":"2023-06-22 19:37:14","model":"DSC-Security","id":2501272,"closed":1,"event":1,"tamper":0,"battery_ok":1,"xactivity":1,"xtamper1":0,"xtamper2":0,"exception":0,"esn":"262a98","status":163,"status_hex":"a3","mic":"CRC","mod":"ASK","freq":433.92666,"rssi":-0.121494,"snr":23.69761,"noise":-23.8191}
-dsc = function( json_ )
-  local id = json_[ 'id' ]
+dsc = function( jvalues_ )
+  local id = jvalues_[ 'id' ]
   local info = dsc_info[ id ]
   if nil ~= info then
     local data = {}
-    extract2( json_, data, "closed",     "")
-    extract2( json_, data, "tamper",     "")
-    extract2( json_, data, "status",     "")
-    extract3( json_, data, "battery_ok", "", "battery_state" )
-    extract2( json_, data, "rssi",       "dBm")
-    extract2( json_, data, "snr",        "")
-    extract2( json_, data, "noise",      "dBm")
+    extract2( jvalues_, data, "closed",     "")
+    extract2( jvalues_, data, "tamper",     "")
+    extract2( jvalues_, data, "status",     "")
+    extract3( jvalues_, data, "battery_ok", "", "battery_state" )
+    extract2( jvalues_, data, "rssi",       "dBm")
+    extract2( jvalues_, data, "snr",        "")
+    extract2( jvalues_, data, "noise",      "dBm")
     mqtt_device_data( object_ptr, info[ 1 ] , info[ 2 ], #data, data );
   else
     local data = {}
-    extract3( json_, data, "battery_ok", "", "battery_state" )
-    extract2( json_, data, "rssi",       "dBm")
-    extract2( json_, data, "snr",        "")
-    extract2( json_, data, "noise",      "dBm")
+    extract3( jvalues_, data, "battery_ok", "", "battery_state" )
+    extract2( jvalues_, data, "rssi",       "dBm")
+    extract2( jvalues_, data, "snr",        "")
+    extract2( jvalues_, data, "noise",      "dBm")
     mqtt_device_data( object_ptr, "top floor", "smoke", #data, data );
   end
 end
@@ -68,18 +68,18 @@ thermapro_location[ 100 ] = 'basement' -- channel 2
 thermapro_location[ 128 ] = 'walkway'  -- channel 3
 
 -- rtl433/2 {"time":"2023-06-22 20:17:22","model":"Prologue-TH","subtype":9,"id":128,"channel":3,"battery_ok":1,"temperature_C":23.5,"humidity":33,"button":0,"mod":"ASK","freq":433.93552,"rssi":-2.04417,"snr":20.27732,"noise":-22.3215}
-thermapro = function( json_ )
-  local id = json_[ 'id' ]
+thermapro = function( jvalues_ )
+  local id = jvalues_[ 'id' ]
   local location = thermapro_location[ id ]
   if nil ~= location then
     local data = {}
-    extract3( json_, data, "temperature_C", "degC", "temperature" )
-    extract2( json_, data, "humidity",      "%")
-    extract3( json_, data, "battery_ok",    "",     "battery_state" )
-    extract3( json_, data, "button",        "",     "button_state" )
-    extract2( json_, data, "rssi",          "dBm")
-    extract2( json_, data, "snr",           "")
-    extract2( json_, data, "noise",         "dBm")
+    extract3( jvalues_, data, "temperature_C", "degC", "temperature" )
+    extract2( jvalues_, data, "humidity",      "%")
+    extract3( jvalues_, data, "battery_ok",    "",     "battery_state" )
+    extract3( jvalues_, data, "button",        "",     "button_state" )
+    extract2( jvalues_, data, "rssi",          "dBm")
+    extract2( jvalues_, data, "snr",           "")
+    extract2( jvalues_, data, "noise",         "dBm")
     mqtt_device_data( object_ptr, location, "thermapro", #data, data );
   end
 end
