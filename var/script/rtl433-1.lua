3d080bb408652b79367d4bcd4f2000782765b8f3
Raymond Burkholder <raymond@burkholder.net>
2023-07-15 13:39:30 -0600

    var/script/rtl433-1.lua - rename constants

--- var/script/rtl433-1.lua
+++ var/script/rtl433-1.lua
@@ -5,10 +5,10 @@
 -- local m = require("strict")
 -- m.lua( true )
 
-description = 'rtl433/1 translation for ws90 and neptune water meter'
+description = 'rtl433/1 915mhz translation for ws90 and neptune water meter'
 
 local topic = 'rtl433/1'
-local object_ptr = 0
+local object_ptr = nil
 
 package.path='lib/lua/*.lua'
 package.cpath='lib/lua/?.so'
@@ -18,7 +18,7 @@ local json = cjson.new()
 local extraction = assert( loadfile( "lib/lua/extract.lua" ) )
 extraction() -- https://www.corsix.org/content/common-lua-pitfall-loading-code
 
-local c_ws90_name = 'ws90'
+local c_name_ws90 = 'ws90'
 local meta_ws90_sensor = {
   --          match            units   common
   { extract3, "temperature_C", "degC", "temperature" },
@@ -40,7 +40,7 @@ local meta_ws90_location_tag = {
   'outside', 'back', 'deck', 'patio'
 }
 
-local c_neptune_name = 'neptune01'
+local c_name_neptune = 'neptune01'
 local meta_neptune_sensor = {
   { extract3, "consumption", "litre", "consumed" }, -- TODO: need to validate precision at other end
   { extract2, "rssi",        "dBm",    "" },
@@ -56,28 +56,28 @@ attach = function ( object_ptr_ )
 
   -- ws90 registration
 
-  device_register_add( object_ptr, c_ws90_name, 'patio ws90' )
+  device_register_add( object_ptr, c_name_ws90, 'patio ws90' )
 
   for key, value in ipairs( meta_ws90_sensor ) do
     -- io.write( 'ws90 key ' .. key .. '=' .. value[ 2 ] .. ',' .. value[ 3 ] .. ',' .. value[ 4 ] .. '\n' )
-    sensor_register_add( object_ptr, c_ws90_name, value[ 2 ], value[ 4 ], value[ 3 ] )
+    sensor_register_add( object_ptr, c_name_ws90, value[ 2 ], value[ 4 ], value[ 3 ] )
   end
 
   for key, value in ipairs( meta_ws90_location_tag ) do
-    device_location_tag_add( object_ptr, c_ws90_name, value )
+    device_location_tag_add( object_ptr, c_name_ws90, value )
   end
 
   -- neptune water meter registration
 
-  device_register_add( object_ptr, c_neptune_name, 'house water' )
+  device_register_add( object_ptr, c_name_neptune, 'house water' )
 
   for key, value in ipairs( meta_neptune_sensor ) do
     -- io.write( 'neptune key ' .. key .. '=' .. value[ 2 ] .. ',' .. value[ 3 ] .. ',' .. value[ 4 ] .. '\n' )
-    sensor_register_add( object_ptr, c_neptune_name, value[ 2 ], value[ 4 ], value[ 3 ] )
+    sensor_register_add( object_ptr, c_name_neptune, value[ 2 ], value[ 4 ], value[ 3 ] )
   end
 
   for key, value in ipairs( meta_neptune_location_tag ) do
-    device_location_tag_add( object_ptr, c_neptune_name, value )
+    device_location_tag_add( object_ptr, c_name_neptune, value )
   end
 
   -- mqtt registration
@@ -90,8 +90,8 @@ detach = function ( object_ptr_ )
   mqtt_stop_topic( object_ptr, topic )
   mqtt_disconnect( object_ptr )
 
-  device_register_del( object_ptr, c_ws90_name ) -- sensors, location tags auto deleted
-  device_register_del( object_ptr, c_neptune_name ) -- sensors, location tags auto deleted
+  device_register_del( object_ptr, c_name_ws90 ) -- sensors, location tags auto deleted
+  device_register_del( object_ptr, c_name_neptune ) -- sensors, location tags auto deleted
 
   object_ptr = nil
 end
@@ -106,7 +106,7 @@ local ws90 = function( jvalues_ )
       local extract = value[ 1 ]
       extract( jvalues_, data, value[ 2 ], value[ 3 ], value[ 4 ] )
     end
-    mqtt_device_data( object_ptr, "patio", c_ws90_name, #data, data );
+    mqtt_device_data( object_ptr, "patio", c_name_ws90, #data, data );
   end
 end
 
@@ -120,7 +120,7 @@ local neptune = function( jvalues_ )
       local extract = value[ 1 ]
       extract( jvalues_, data, value[ 2 ], value[ 3 ], value[ 4 ] )
     end
-    mqtt_device_data( object_ptr, "house", c_neptune_name, #data, data );
+    mqtt_device_data( object_ptr, "house", c_name_neptune, #data, data );
   end
 end
 
